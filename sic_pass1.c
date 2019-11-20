14f300506606b8c0dd48a7ce7604f15f40614cf2
besh612 <sung.hun5016@gmail.com>
2019-11-21 01:58:11 +0900

    fix error on sic_pass1

--- sic_pass1.c
+++ sic_pass1.c
@@ -20,72 +20,52 @@ int pass1_main()
 {
     int LOCCTR = 0;
     int flag = 1;
-    int size = 1;
-    int value = 3;
+    int found = 2;
 
     FILE *open_sic_codes = fopen("input/input.sic", "r");
     FILE *save_after_pass1 = fopen("output/output_pass1.txt", "w");
 
     while (1)
     {
+        int size = 1;
         read_line(open_sic_codes, 1);
-        if (!strcmp(LABEL, "."))
+
+        //if OPCODE = 'START'
+        if (strcmp(OPCODE, "START") == 0)
         {
+            char *END;
+            //strtol: 16진법으로 표기된 문자열을 정수로 변환
+            START_LOC = (int)strtol(OPERANDS, NULL, 16);
+            LOCCTR = (int)strtol(OPERANDS, &END, 16);
+            flag = 0;
             continue;
         }
+        else if (strcmp(LABEL, ".") == 0)
+            continue;
         else
         {
-            if (!strcmp(OPCODE, "START"))
-            {
-                if (flag == 1)
-                {
-                    char *END;
-                    START_LOC = (int)strtol(OPERANDS, &END, 16);
-                    LOCCTR = (int)strtol(OPERANDS, &END, 16);
-                    flag = 0;
-                }
-                else if (flag == 0)
-                {
-                    printf("ERROR - 1\n");
-                    exit(1);
-                }
-            }
-
+            //if this is not a comment line (LABEL 존재 파악)
             if (LABEL[0] != '\0')
             {
-                value = sym_tab_check();
-                if (value == 0)
+                found = sym_tab_check();
+                if (found == 0)
                 {
-                    printf("value is 0\n");
                     add_sym_tab();
                 }
                 else
                 {
-                    printf("ERROR - 2\n");
+                    printf("ERROR - 1 duplicate symbol\n");
                     exit(1);
                 }
             }
         }
 
-        if (strcmp(OPCODE, "END") == 0)
+        if (search_opcode_tab())
+            LOCCTR = LOCCTR + 3;
+        else if (strcmp(OPCODE, "BYTE") == 0)
         {
-            LENGTH = LOCCTR - START_LOC;
-            fprintf(save_after_pass1, "\t%s\t%s\t%s\n", LABEL, OPCODE, OPERANDS);
-            break;
-        }
-        fprintf(save_after_pass1, "%d\t%s\t%s\t%s\n", LOCCTR, LABEL, OPCODE, OPERANDS);
-
-        if (feof(open_sic_codes))
-        {
-            printf("END\n");
-            exit(1);
-        }
-
-        if (!strcmp(OPCODE, "BYTE"))
-        {
-            if (!atoi(OPERANDS))
+            if (atoi(OPERANDS) == 0)
             {
-                int res;
                 int i;
                 for (i = 0; i < SIZE; i++)
                 {
@@ -93,31 +73,42 @@ int pass1_main()
                         break;
                 }
                 if (OPERANDS[0] == 'X')
-                    res = 1;
+                    size = 1;
                 else
-                    res = i - 3;
-                size = res;
+                    size = i - 3;
                 LOCCTR = LOCCTR + size;
-            }
-            else if (!strcmp(OPCODE, "WORD") || search_opcode_tab())
-                LOCCTR = LOCCTR + 3;
-            else if (!strcmp(OPCODE, "RESW"))
-                LOCCTR = LOCCTR + atoi(OPERANDS) * 3;
-            else if (!strcmp(OPCODE, "RESB"))
-            {
-                size = atoi(OPERANDS);
-                LOCCTR = LOCCTR + size;
-            }
-            else if (!strcmp(OPCODE, "END"))
-                printf("PASS1 종료\n");
-            else
-            {
-                printf("ERROR - 3\n");
-                exit(1);
             }
         }
+        else if (strcmp(OPCODE, "WORD") == 0)
+            LOCCTR = LOCCTR + 3;
+        else if (strcmp(OPCODE, "RESW") == 0)
+        {
+            size = atoi(OPERANDS) * 3;
+            LOCCTR = LOCCTR + size;
+        }
+        else if (strcmp(OPCODE, "RESB") == 0)
+        {
+            size = atoi(OPERANDS);
+            LOCCTR = LOCCTR + size;
+        }
+        else if (strcmp(OPCODE, "END") == 0)
+        {
+            LENGTH = LOCCTR - START_LOC;
+            printf("is end\nLENGTH[%d] = LOCCTR[%d] - START_LOC[%d]\n", LENGTH, LOCCTR, START_LOC);
+            fprintf(save_after_pass1, "\t%s\t%s\t%s\n", LABEL, OPCODE, OPERANDS);
+            break;
+        }
+        else
+        {
+            printf("ERROR - 2 invalid operation code\n");
+            exit(1);
+        }
+        printf("%d\t%s\t%s\t%s\n", LOCCTR, LABEL, OPCODE, OPERANDS);
+        fprintf(save_after_pass1, "%d\t%s\t%s\t%s\n", LOCCTR, LABEL, OPCODE, OPERANDS);
     }
+
     fclose(open_sic_codes);
     fclose(save_after_pass1);
+
     return 0;
 }
--- utils.c
+++ utils.c
@@ -67,7 +67,7 @@ int sym_tab_check()
     SYMTAB *data = SYMLIST;
     while (data != NULL)
     {
-        if (!strcmp(LABEL, data->LABEL))
+        if (strcmp(LABEL, data->LABEL) == 0)
         {
             return 1;
         }
@@ -75,7 +75,7 @@ int sym_tab_check()
     }
     return 0;
 }
-
+/*
 void make_object_code(char *temp)
 {
     char code[7];
@@ -90,13 +90,14 @@ void make_object_code(char *temp)
     change_uppercase(code);
     strcpy(temp, code);
 }
+*/
 
 int search_opcode_tab()
 {
     OPCODETAB *temp = OPCODELIST;
     while (temp != NULL)
     {
-        if (!strcmp(temp->NAME, OPCODE))
+        if (strcmp(temp->NAME, OPCODE) == 0)
         {
             HEXNUM = temp->HEXNUM;
             return 1;
