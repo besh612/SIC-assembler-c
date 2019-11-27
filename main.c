844b5a380133a3e87ff061d08b4e931c5017f368
Besh612 <sung.hun5016@gmail.com>
2019-11-27 18:47:03 +0900

    add argc, argv when start main

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
-#include "myLib.h"
+#include "groupL_LIB.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        fputs("error: no input file found\n", stderr);
+        exit(1);
+    }
     init_optable();
-    pass1_main();
+    pass1_main(argv[1]);
+    read_pass1_output();
+    output_pass2();
 }
